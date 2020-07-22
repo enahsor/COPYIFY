90abf47d4b3862cae22411a488a4ec5ad603167f
Dowen Robinson <drobinson@ad.accelclaims.com>
2020-07-22 09:13:13 -0500

    Added data class

---
+++ src/socket/index.js
@@ -0,0 +1,29 @@
+import io from 'socket.io-client'
+import ss from 'socket.io-stream'
+import { URL } from '../config'
+import { Data } from '../dataQueue/data'
+import { Player } from '../dataQueue'
+import { getCtx } from '../audio'
+
+export const client = io(URL)
+
+export function callForTrack(trackid) {
+    client.emit('send-track', trackid)
+}
+
+const ctx = getCtx()
+const player = new Player(ctx)
+
+ss(client).on('sending', (stream) => {
+    player.init()
+    player.clearQueue()
+    stream.on('data', async (byte) => {
+        const data = new Data(ctx, byte)
+        try {
+            player.addToQueue(await data.decode())
+            //player.playAvailableSlices()
+        } catch (err) {
+            //DO SOMETHING HERE
+        }
+    })
+})
